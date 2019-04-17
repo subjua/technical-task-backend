1cbc5cd35622ad83b902bb65c27159ecf1809618
Anton Yashyn <anton@MacBook-Pro-Anton.local>
2019-04-17 17:20:07 +0300

    clean code

--- midlewares/cors.js
+++ midlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = async (ctx, next) => {
   const origin = ctx.headers.origin
 
-  ctx.set('Access-Control-Allow-Origin', origin)
+  ctx.set('Access-Control-Allow-Origin', "*")
   ctx.set('Access-Control-Allow-Methods', '*')
   ctx.set('Access-Control-Allow-Headers', '*')
   ctx.set('Access-Control-Allow-Credentials', 'true')
